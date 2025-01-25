10a118095cee442a123cbccdb5abbac6a602e01a
Davi <davianitelli282@hotmail.com>
2025-01-25 13:52:45 -0300

    refact(employee): refactoring minimal API with controller

---
+++ EmployeeAPI/BaseController.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAPI;
+
+[ApiController]
+[Route("[controller]")]
+public abstract class BaseController : Controller
+{
+    protected async Task<ValidationResult> ValidateAsync<T>(T instance)
+    {
+        var validator = HttpContext.RequestServices.GetService<IValidator<T>>() 
+            ?? throw new ArgumentException($"No validator found for {typeof(T).Name}");
+        var result = await validator.ValidateAsync(instance);
+        return result;
+    }
+}
